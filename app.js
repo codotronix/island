11477d5bb5e8c93502cad7ae7434f6078aca7f39
codotronix <codotronix@gmail.com>
2015-06-02 23:46:24 +0530

    added more things

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var express = require('express'),
     firebase = require('firebase'),
-    app = express();
-    
+    app = express(),
+    cons = require('consolidate');
+ 
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+
 //my firebase ref
-var myFirebaseRef = new firebase("https://crackling-inferno-4598.firebaseio.com/hobby");
+var myFirebaseRef = new firebase("https://crackling-inferno-4598.firebaseio.com/");
     
 app.get('/', function(req, res){
-    myFirebaseRef.set({
-        "id1":"Suman",
+	/*
+    myFirebaseRef.push([{
+        "time":"Suman" + (new Date()),
         "id2":"Tulu"
-    });
+    }]);
     res.send('Hello from Node Express running in Cloud 9...<br/>Some data is written to firebase...');
+    */
+
+    res.render('hello', {"name":"Suman Barick"});
 });
 app.listen(9090);
 console.log('Node Express Server running at localhost:9090');
